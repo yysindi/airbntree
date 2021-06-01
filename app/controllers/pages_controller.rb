d3ac15f41e14439ead9b2ecb38963639a99a1219
Ariella Morris <ariellax@gmail.com>
2021-06-01 15:22:54 +0100

    profile creation and styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def profile
+    if current_user
+      @bookings = current_user.bookings
+      @treehouses = current_user.treehouses
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'bookings/create'
   devise_for :users
   root to: 'pages#home'
+  get 'profile', to: 'pages#profile'
   resources :treehouses do 
     resources :bookings, only: [:create, :new]
   end 
