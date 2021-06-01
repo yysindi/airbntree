c2fb16f021080b0909d535f7e877a7c988ca8792
steve fernandes <stevefernandes24@hotmail.co.uk>
2021-06-01 16:35:04 +0100

    added validations

--- app/models/treehouse.rb
+++ app/models/treehouse.rb
@@ -1,4 +1,9 @@
 class Treehouse < ApplicationRecord
   has_many :bookings
   belongs_to :user
+
+  validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+  validates :price, numericality: { only_integer: true }, presence: true
+  validates :description, length: { minimum: 30 }, presence: true
+  validates :location, presence: true
 end
