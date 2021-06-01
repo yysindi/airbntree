b3e0eea375912b1ccc67875bda97c1f3ba412f7a
steve fernandes <stevefernandes24@hotmail.co.uk>
2021-06-01 15:22:16 +0100

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email]) 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
   end
 end
--- app/models/treehouse.rb
+++ app/models/treehouse.rb
@@ -1,5 +1,4 @@
 class Treehouse < ApplicationRecord
   has_many :bookings
-  has_many :users, through: :bookings
-  belongs_to :users
+  belongs_to :user
 end
