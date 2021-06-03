674a16fb16fd41a8d27aba006fb4854d8f16cf62
Yusuf Sindi <yysindi@ymail.com>
2021-06-03 12:21:54 +0100

    search bar with pg search gem

--- app/models/treehouse.rb
+++ app/models/treehouse.rb
@@ -7,4 +7,11 @@ class Treehouse < ApplicationRecord
   validates :price, numericality: { only_integer: true }, presence: true
   validates :description, length: { minimum: 30 }, presence: true
   validates :location, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_location_and_description,
+    against: [:location, :description],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
