c6aadd546a8cf55646c7a6fa0794d74c88d363c3
steve fernandes <stevefernandes24@hotmail.co.uk>
2021-06-04 11:55:26 +0100

    offer has been updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def offers
+    Booking.where(treehouse_id: self.treehouses.ids).count
+  end
 end
