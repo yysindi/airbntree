5049b00abbcec6bae8835ab226c6466d72d17f14
Ariella Morris <ariellax@gmail.com>
2021-06-13 08:18:13 +0100

    fixed js for booking

--- app/javascript/components/form-logic.js
+++ app/javascript/components/form-logic.js
@@ -4,29 +4,32 @@ const formLogic = () => {
 
   if ( checkIn ){
     let checkOut = document.getElementById('booking_check_out');
-    let pricePerNight = document.getElementById('price-p-night').textContent;
+    const pricePerNight = document.getElementById('price-p-night').textContent;
     const totalPrice = document.getElementById('total-price');
     const totalP = document.getElementById('booking_total_price');
-    let cIn = "";
-    let out ="";
+    let dateIn = "";
+    let dateOut = "";
+    let finalPrice = 0;
+
 
     checkIn.addEventListener('blur', (event) => {
-      checkIn = event.target.value;
-      cIn = checkIn.toString().replace(/-/g, "");
-      
+      checkIn = event.target.value.toString().split('-');
+      dateIn = new Date(checkIn[0], checkIn[1]-1, checkIn[2]);
     });
+
     checkOut.addEventListener('blur', (event) => {
-      checkOut = event.target.value;
-      out = checkOut.toString().replace(/-/g, "");
-      const finalPrice = (out - cIn) * pricePerNight;
-      // totalPrice.innerText = "Total Price: £ " + finalPrice;
-      totalP.value =  finalPrice;
+      checkOut = event.target.value.toString().split('-');
+      dateOut = new Date(checkOut[0], checkOut[1]-1, checkOut[2]);
+      let difference = dateOut.getTime() - dateIn.getTime();
+      let days = Math.ceil(difference / (1000 * 3600 * 24));
+      finalPrice = pricePerNight * days;
+      totalP.value = finalPrice;
       totalPrice.innerHTML =  finalPrice;
+      
+      console.log(totalP)
     });
+
   }
-
-
-
 }
 
 
